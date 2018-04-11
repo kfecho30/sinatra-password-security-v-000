a1397df15152bbe0cc407038105e3b1da17fb49a
Kelsey Fecho <kfecho30@gmail.com>
2018-04-11 22:38:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
+		if user && yser.authenticate(params[:password])
+			session[:user_id] = user.id
 			redirect "/success"
 		else
 			redirect "/failure"
